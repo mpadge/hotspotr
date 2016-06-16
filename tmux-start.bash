447918e9902fed4bf53aca2f450a27b1fc8fdc89
mpadge <mark.padgham@email.com>
2016-06-16 14:24:55 +0200

    add tmux-start

---
+++ tmux-start.bash
@@ -0,0 +1,26 @@
+#!/bin/sh
+SESSION="hotspotr"
+
+tmux -2 new-session -d -s $SESSION
+
+tmux new-window -t $SESSION:1 -k -n main
+tmux send-keys -t $SESSION:1 'vim README.Rmd' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe R/neutral1d.R' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe R/neutral2d.R' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe R/test1d.R' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe R/test2d.R' C-m
+tmux send-keys -t $SESSION:1 ':' 'tabe R/run_tests.R' C-m
+#tmux send-keys -t $SESSION:1 '2gt'
+tmux select-pane -t 0
+
+tmux new-window -t $SESSION:2 -n makefile
+tmux select-window -t $SESSION:2
+tmux send-keys -t $SESSION:2 'vim tmux-start.bash' C-m
+tmux send-keys -t $SESSION:2 ':' 'tabe makefile' C-m
+tmux split-window -h
+tmux send-keys -t $SESSION:2 'git status -uno' C-m
+tmux select-pane -t 0
+
+tmux select-window -t $SESSION:1
+
+tmux attach -t $SESSION

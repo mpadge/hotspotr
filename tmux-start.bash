8c519efd579477fb4ef4af8db63c791dcdc070d0
mpadge <mark.padgham@email.com>
2016-06-20 13:15:46 +0200

    update tmux-start

--- tmux-start.bash
+++ tmux-start.bash
@@ -3,13 +3,13 @@ SESSION="hotspotr"
 
 tmux -2 new-session -d -s $SESSION
 
-tmux new-window -t $SESSION:1 -k -n main
+tmux new-window -t $SESSION:1 -k -n R
 tmux send-keys -t $SESSION:1 'vim README.Rmd' C-m
 tmux send-keys -t $SESSION:1 ':' 'tabe R/test2d.R' C-m
 tmux send-keys -t $SESSION:1 ':' 'tabe R/run_tests.R' C-m
 #tmux send-keys -t $SESSION:1 '2gt'
 
-tmux new-window -t $SESSION:2 -k -n main
+tmux new-window -t $SESSION:2 -k -n cpp
 tmux send-keys -t $SESSION:2 'cd ./src' C-m
 tmux send-keys -t $SESSION:2 'vim neutral2d.cpp' C-m
 tmux send-keys -t $SESSION:2 ':' 'tabe ives2d.cpp' C-m

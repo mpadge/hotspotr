8bb0872ebd579707221812a7fedc18fb275211d7
mpadge <mark.padgham@email.com>
2016-06-21 16:32:27 +0200

    construct neighbour lists in .cpp rather than R

--- R/get-neighbours.R
+++ R/get-neighbours.R
@@ -4,7 +4,6 @@
 #'
 #' @param x Vector of x-coordinates
 #' @param y Vector of y-coordinates
-#' @param plot Produce a plot of the Delaunay triangulation
 #'
 #' @return A list of Delaunay neighbours of each point in (x, y)
 #'
@@ -24,15 +23,5 @@ get_neighbours <- function (x, y)
         y <- y [seq (len)]
     }
 
-    nbs <- do.call (rbind, rcpp_get_neighbours (x=x, y=y))
-    # nbs is a list of triangular neighbours for each point. This is converted
-    # to list of *all* neighbours for each point
-    nbs2 <- list ()
-    for (i in seq (length (x)))
-    {
-        indx <- which (apply (nbs, 1, function (j) i %in% j))
-        pts <- sort (unique (as.numeric (nbs [indx,])))
-        nbs2 [[i]] <- pts [!pts %in% i]
-    }
-    return (nbs2)
+    rcpp_get_neighbours (x=x, y=y)
 }
--- src/get-neighbours.cpp
+++ src/get-neighbours.cpp
@@ -26,25 +26,78 @@ Rcpp::List rcpp_get_neighbours (Rcpp::NumericVector x, Rcpp::NumericVector y)
     std::vector< std::pair<Point, unsigned> > points;
     unsigned number = 0;
     for (int i=0; i<x.size (); i++)
-        points.push_back ( std::make_pair (Point (x (i), y (i)), ++number) );
+        points.push_back ( std::make_pair (Point (x (i), y (i)), number++) );
 
     Delaunay triangulation;
     triangulation.insert (points.begin (), points.end ());
 
     Rcpp::List nbs;
-    std::vector <int> nbsi;
-    nbsi.resize (0);
+
+    int ntri = 0;
+    for(Delaunay::Finite_faces_iterator fit = triangulation.finite_faces_begin();
+            fit != triangulation.finite_faces_end(); ++fit) 
+        ntri++;
+
+    Rcpp::NumericMatrix triangles (ntri, 3);
+    ntri = 0;
     for(Delaunay::Finite_faces_iterator fit = triangulation.finite_faces_begin();
             fit != triangulation.finite_faces_end(); ++fit) 
     {
-        nbsi.resize (0);
         Delaunay::Face_handle face = fit;
         for (int i=0; i<3; i++) 
-            nbsi.push_back (face -> vertex (i) -> info ());
+            triangles (ntri, i) = face -> vertex (i) -> info ();
+        ntri++;
+    }
+
+    /* These triangles are then converted to lists of neighbouring points. The
+     * following code is easily implemented in R, assuming rcpp_get_neighbours
+     * just returns a list of triangular neighbours (that is, triangles in the
+     * above constructed as an Rcpp::List instead of Rcpp::Matrix):
+     * > nbs <- do.call (rbind, rcpp_get_neighbours (x=x, y=y))
+     * > nbs2 <- list ()
+     * > for (i in seq (length (x)))
+     * > {
+     * >     indx <- which (apply (nbs, 1, function (j) i %in%
+     * > )
+     * >     pts <- sort (unique (as.numeric (nbs [indx,])))
+     * >     nbs2 [[i]] <- pts [!pts %in% i]
+     * > }
+     * ----------------- microbenchmark results:
+     *  R = 62.8ms, Rcpp = 0.34ms
+     * The Rcpp speed-up is over 99.5%!!!
+     */
+
+    int tempi;
+    std::vector <int> nbsi;
+    bool i_in_list;
+
+    for (int i=0; i<x.size (); i++)
+    {
+        nbsi.resize (0);
+        for (int j=0; j<triangles.nrow (); j++)
+        {
+            i_in_list = false;
+            for (int k=0; k<3; k++)
+                if (triangles (j, k) == i)
+                    i_in_list = true;
+            if (i_in_list)
+                for (int k=0; k<3; k++)
+                {
+                    tempi = triangles (j, k);
+                    if (tempi != i && std::find (nbsi.begin (), nbsi.end (),
+                                tempi) == nbsi.end ())
+                        nbsi.push_back (tempi);
+                }
+        } // end for j over nrow (triangles)
+        std::sort (nbsi.begin (), nbsi.end ());
+        // Then adjust nbsi by 1 to make them R-indexed
+        for (std::vector <int>::iterator it=nbsi.begin (); 
+                it != nbsi.end (); ++it)
+            *it = *it + 1;
         nbs.push_back (nbsi);
     }
-    nbsi.resize (0);
 
+    nbsi.resize (0);
     points.resize (0);
 
     return nbs;

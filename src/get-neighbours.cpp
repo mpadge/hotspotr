ffb0bc3ef91d4a42621fe2b980925ed4963a647e
mpadge <mark.padgham@email.com>
2016-06-21 14:52:43 +0200

    clean up get-neighbours.cpp

--- src/get-neighbours.cpp
+++ src/get-neighbours.cpp
@@ -10,13 +10,6 @@ typedef CGAL::Triangulation_data_structure_2<Vb>                       Tds;
 typedef CGAL::Delaunay_triangulation_2<Kernel, Tds>                    Delaunay;
 typedef Kernel::Point_2                                                Point;
 
-typedef Tds::Vertex Vertex;
-typedef Tds::Vertex_iterator Vertex_iterator;
-typedef Tds::Face Face;
-typedef Tds::Face_iterator Face_iterator;
-typedef Face::Face_handle Face_handle;
-typedef Face::Vertex_handle Fvertex_handle;
-
 // [[Rcpp::export]]
 Rcpp::List rcpp_get_neighbours (Rcpp::NumericVector x, Rcpp::NumericVector y)
 {
@@ -24,21 +17,11 @@ Rcpp::List rcpp_get_neighbours (Rcpp::NumericVector x, Rcpp::NumericVector y)
     std::vector< std::pair<Point, unsigned> > points;
     unsigned number = 0;
     for (int i=0; i<x.size (); i++)
-    {
         points.push_back ( std::make_pair (Point (x (i), y (i)), ++number) );
-    }
 
     Delaunay triangulation;
     triangulation.insert (points.begin (), points.end ());
 
-    /* This first way of extracting information uses a faces iterator to extract the
-     * vertex->info for each face. A Delaunay triangulation has both finite and
-     * infinite faces (see CGAL documentation). The infinite faces join to an
-     * external, infinite vertex, so the finite_faces_iterator just includes the
-     * internal faces. 
-     *
-     * mircobenchmark (times=1000L for 100 points): 61.339ms
-     */
     Rcpp::List nbs;
     std::vector <int> nbsi;
     nbsi.resize (0);
@@ -52,42 +35,6 @@ Rcpp::List rcpp_get_neighbours (Rcpp::NumericVector x, Rcpp::NumericVector y)
         nbs.push_back (nbsi);
     }
     nbsi.resize (0);
-
-    /* And this is a long-hand way, using an iteration over all faces, checking
-     * whether they are finite or not, and accessing the face.vertex info by
-     * dereferencing pointers to each vertex of the face. 
-     *
-     * mircobenchmark (times=1000L for the same 100 points): 61.623ms
-     */
-    /*
-    Vertex v;
-    Face_iterator itf = triangulation.faces_begin (),
-                  beyondf = triangulation.faces_end ();
-    Face face;
-    Face_handle neighbour;
-    Fvertex_handle fvertex;
-
-    Delaunay tr_reduced;
-    bool is_finite;
-    while (itf != beyondf) 
-    {
-        nbsi.resize (0);
-        face = *(itf++);
-        is_finite = true;
-        for (int i=0; i<3; i++) 
-            if (triangulation.is_infinite (face.vertex (i))) 
-                is_finite = false;
-        if (is_finite)
-        {
-            for (int i=0; i<3; i++) 
-                nbsi.push_back ((*(face.vertex (i))).info ());
-                //v = *(face.vertex (i));
-                //nbsi.push_back (v.info ());
-            nbs.push_back (nbsi);
-        }
-    }
-    nbsi.resize (0);
-    */
 
     points.resize (0);
 
